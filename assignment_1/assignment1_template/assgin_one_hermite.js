816995e8603505469508888efa856e6b6951d1bb
Kevin Alvarez <kevinalvarezcampos@gmail.com>
2026-01-26 18:55:39 -0800

    Added a testing comment

--- assignment_1/assignment1_template/assgin_one_hermite.js
+++ assignment_1/assignment1_template/assgin_one_hermite.js
@@ -1,6 +1,6 @@
 
 import {tiny, defs} from './examples/common.js';
-
+// test addition
 // Pull these names into this module's scope for convenience:
 const { vec3, vec4, color, Mat4, Shape, Material, Shader, Texture, Component } = tiny;
 
